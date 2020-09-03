25e24ce3478c47461c43e944ce7b9455d4e4b5c2
Omar Tan <omartan90@hotmail.com>
2020-09-03 16:36:05 +0800

    Linked UIElements to ViewController

--- Unsplash Random Demo/ViewController.swift
+++ Unsplash Random Demo/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var userLabel: UILabel!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var reloadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        reloadButton.layer.cornerRadius = 25
     }
 
-
+    @IBAction func getPhoto(_ sender: UIButton) {
+    }
+    
 }
 
