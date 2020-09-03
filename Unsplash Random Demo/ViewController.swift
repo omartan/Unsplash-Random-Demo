631566948854ae8d57fed3f93aea06784700388a
Omar Tan <omartan90@hotmail.com>
2020-09-03 16:17:51 +0800

    Initial Commit

---
+++ Unsplash Random Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unsplash Random Demo
+//
+//  Created by Omar Tan Johan Tan on 03/09/2020.
+//  Copyright © 2020 Omar Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
